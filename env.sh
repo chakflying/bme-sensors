fc35dc0cc98463e382be77843b5e6efe7506b0c4
Nelson Chan <chakflying@hotmail.com>
2023-01-08 01:22:11 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get install build-essential git dnsutils clang
+sudo apt-get install -y build-essential git dnsutils clang
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -fsSL https://tailscale.com/install.sh | sh
